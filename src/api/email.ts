53db17758ef1a8784afd7b4585b01fec87efb750
Aldo Soto <asoto@ince.con.mx>
2026-02-23 10:39:45 -0800

    add enviroment folder

--- src/api/email.ts
+++ src/api/email.ts
@@ -1,8 +1,13 @@
+import { environment } from '../environment/enviroment'
 
-const BASE_URL = "http://127.0.0.1:8000/api/verificacion/send-email-service-request/"
+const URI = environment.BASE_URL;
+const requestVerificationEmailEndPoint = '/api/verificacion/send-email-service-request/'
+
+const BASE_URL_EMAIL = `${URI}${requestVerificationEmailEndPoint}`
+
 export const requestVerificationEmail = async (form:FormData) => {
     return await fetch(
-        BASE_URL, 
+        BASE_URL_EMAIL, 
         {
             method: "POST", 
             body: form
